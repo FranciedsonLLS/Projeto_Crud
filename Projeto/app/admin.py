48edb3cccd7a96cdc854d56b9389a2a0fc0cffe3
MisacDutra <100878742+MisacDutra@users.noreply.github.com>
2022-12-20 15:51:53 -0300

    Delete Projeto directory

--- Projeto/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import Cliente, Filme, Reserva
-
-
-admin.site.register(Cliente)
-admin.site.register(Filme)
-admin.site.register(Reserva)
-# Register your models here.
